da77b16524b8b383959b68dad23946230e5ab4ca
zishine <wzishine@gmail.com>
2026-01-09 04:20:02 -0500

    fix icon

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import favicon from './assets/img_0010.png'
+import faviconImg from '../img_0010.png'
 
 // Set favicon
 const link = document.createElement('link')
 link.rel = 'icon'
-link.href = favicon
+link.href = faviconImg
 document.head.appendChild(link)
 
 createRoot(document.getElementById('root')).render(
